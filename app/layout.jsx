577b53d151700c832ee7b380ae4deb120084f1b0
Niket Chawla <104648509+NC0312@users.noreply.github.com>
2024-09-29 20:56:26 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,33 +1,3 @@
-// import { JetBrains_Mono } from "next/font/google";
-// import "./globals.css";
-// import Header from "@/components/Header";
-// import { icons } from "lucide-react";
-// // import PageTransition from "@/components/PageTransition";
-
-// const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"] , weight:["100","200","300","400","500","600","700","800"],
-//   variable:'--font-jetBrainsMono'
-// });
-
-// export const metadata = {
-//   title: "Niket .",
-//   description: "This is my portfolio website made using NextJS.",
-//   icons: {
-//     icon: '/assets/logo.png',
-//   },
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={jetbrainsMono.variable}>
-//         <Header/> 
-//         {children}
-
-//         </body>
-//     </html>
-//   );
-// }
-
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -40,7 +10,7 @@ const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"] , weight:["100","200",
 });
 
 export const metadata = {
-  title: "Niket . Chawla",
+  title: "Niket Chawla",
   description: "This is my portfolio website Niket Chawla made using NextJS.",
   icons: {
     icon: '/assets/logo.png',
