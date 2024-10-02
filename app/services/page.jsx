6d239e10cf7417880aecc5e9c5610edeae0b63ab
Niket Chawla <104648509+NC0312@users.noreply.github.com>
2024-10-02 21:34:18 +0530

    Update page.jsx
    service section updated

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -10,27 +10,28 @@ const services=[
         title:'MERN Stack Web Development',
         description:'Expert in building full-stack web applications using MongoDB, Express.js, React, and Node.js. Delivering scalable, efficient, and modern web solutions tailored to your specific needs.'
     },
-
     {
         num:'02',
-        title:'NextJS Web Developer',
+        title:'Full Stack Java Development',
+        description:'Experienced in full stack development using Java, Spring Boot, and Hibernate. Proficient in building scalable backend RESTful APIs, and seamless front-end integration.',
+    },
+
+    {
+        num:'03',
+        title:'NextJS Web Development',
         description:'Specialized in creating high-performance, SEO-friendly web applications using Next.js. Leveraging server-side rendering and static site generation for optimal user experience and faster load times.'
     },
     {
-        num:'03',
-        title:'Website Designer',
+        num:'04',
+        title:'UI Development',
         description:'Crafting visually stunning and responsive websites using CSS, Tailwind CSS, and other modern design frameworks. Skilled in creating intuitive user interfaces that blend aesthetics with functionality for an optimal user experience.'
     },
-    {
-        num:'04',
-        title:'Core Java Developer',
-        description:'Experienced in developing robust, scalable applications using core Java. Proficient in object-oriented programming, multithreading, collections, and Java SE APIs to create efficient, maintainable software solutions.',
-    },
-    {
-        num:'05',
-        title:'React Native Developer',
-        description:'Skilled in developing cross-platform mobile applications with React Native, specializing in Android app creation. Leveraging React principles to build efficient, native-like mobile experiences with a single codebase.',
-    }
+    
+    // {
+    //     num:'05',
+    //     title:'React Native Development',
+    //     description:'Skilled in developing cross-platform mobile applications with React Native, specializing in Android app creation. Leveraging React principles to build efficient, native-like mobile experiences with a single codebase.',
+    // }
     
 
 ]
@@ -52,7 +53,9 @@ const Services = () => {
                     return <div key={index} className='flex-1 flex flex-col justify-center gap-6 group'>
                         <div className='w-full flex justify-between items-center'>
                             <div className='text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover'>{service.num}</div>
-                            <a href='https://www.fiverr.com/s/BRmxd4d' target='_blank' rel='noopener noreferrer'  className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
+                            <a  href='https://www.fiverr.com/s/BRmxd4d' 
+  target='_blank' 
+  rel='noopener noreferrer'  className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
                             <BsArrowDownRight className='text-primary text-3xl'/>
                             </a>
                         </div>
