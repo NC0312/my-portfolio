0d7e40e34503386d8058a509a4585a2eda54f887
Niket Chawla <104648509+NC0312@users.noreply.github.com>
2024-10-06 12:04:32 +0530

    Update page.jsx
    company logo and socials added in experience section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { FaReact, FaNodeJs, FaDatabase, FaJava, FaJs, FaCss3, FaHtml5, FaDocker,FaGit, FaFigma, FaAws, FaStripe } from 'react-icons/fa';
-import { SiTailwindcss, SiNextdotjs,SiJira} from 'react-icons/si';
+import { FaReact, FaNodeJs, FaDatabase, FaJava, FaJs, FaCss3, FaHtml5, FaDocker, FaGit, FaFigma, FaAws, FaStripe } from 'react-icons/fa';
+import { SiTailwindcss, SiNextdotjs, SiJira } from 'react-icons/si';
 
 const about = {
   title: "About me",
@@ -46,9 +46,11 @@ const experience = {
 
   items: [
     {
-      company:'Twinline Business Solutions',
-      position:'Full Stack Java Developer',
-      duration:'Aug 2024 - Present'
+      company: 'Twinline Business Solutions',
+      position: 'Full Stack Java Developer',
+      duration: 'Aug 2024 - Present',
+      logo: '/assets/company1.png',
+      linkedin: 'https://www.linkedin.com/company/twinline-business-solutions/'
     },
   ]
 }
@@ -60,144 +62,157 @@ const education = {
     {
       institution: "Chitkara University",
       position: "BTech-CSE",
-      cgpa:"CGPA - 9.50"
+      cgpa: "CGPA - 9.50"
     },
     {
-      institution:"R.S.D Raj Rattan Public School",
-      position:"Senior-Secondary",
-      cgpa:"Percentage - 81.8%"
-    },{
-      institution:"DCM International School",
-      position:"Secondary",
-      cgpa:"Percentage - 79.8%"
+      institution: "R.S.D Raj Rattan Public School",
+      position: "Senior-Secondary",
+      cgpa: "Percentage - 81.8%"
+    }, {
+      institution: "DCM International School",
+      position: "Secondary",
+      cgpa: "Percentage - 79.8%"
     }
   ]
 }
 
-const skills={
-  title:"My Skills",
-  skillList:[
+const skills = {
+  title: "My Skills",
+  skillList: [
     {
-      icon:<FaJava/>,
-      name:"Java/Springboot/Hibernate",
+      icon: <FaJava />,
+      name: "Java/Springboot/Hibernate",
     },
     {
-      icon:<SiNextdotjs/>,
-      name:"Next JS",
+      icon: <SiNextdotjs />,
+      name: "Next JS",
     },
     {
-      icon:<FaReact/>,
-      name:"React JS",
+      icon: <FaReact />,
+      name: "React JS",
     },
     {
-      icon:<FaNodeJs/>,
-      name:"Node JS",
+      icon: <FaNodeJs />,
+      name: "Node JS",
     },
     {
-      icon:<FaDatabase/>,
-      name:"Mongo DB",
+      icon: <FaDatabase />,
+      name: "Mongo DB",
     },
-    
+
     {
-      icon:<FaFigma/>,
-      name:"Figma",
-    },
-    {
-      icon:<SiTailwindcss/>,
-      name:"Tailwind CSS",
+      icon: <FaFigma />,
+      name: "Figma",
     },
     {
-      icon:<FaJs/>,
-      name:"Javascript",
+      icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
     },
     {
-      icon:<FaAws/>,
-      name:'AWS',
-    },
-    // {
-    //   icon:<FaStripe/>,
-    //   name:'Stripe',
-    // },
-    
-    {
-      icon:<FaGit/>,
-      name:"Git",
-    },
-   
-    // {
-    //   icon:<SiJira/>,
-    //   name:"Jira",
-    // },
-    // {
-    //   icon:<FaDocker/>,
-    //   name:"Docker",
-    // },
-    {
-      icon:<FaCss3/>,
-      name:"CSS3",
+      icon: <FaJs />,
+      name: "Javascript",
     },
     {
-      icon:<FaHtml5/>,
-      name:"HTML5",
+      icon: <FaAws />,
+      name: 'AWS',
+    },
+    {
+      icon: <FaStripe />,
+      name: 'Stripe',
+    },
+
+    {
+      icon: <FaGit />,
+      name: "Git",
+    },
+
+    {
+      icon: <SiJira />,
+      name: "Jira",
+    },
+    {
+      icon: <FaDocker />,
+      name: "Docker",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS3",
+    },
+    {
+      icon: <FaHtml5 />,
+      name: "HTML5",
     },
   ]
 }
 
-import {Tabs,TabsContent,TabsList,TabsTrigger} from '@/components/ui/tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
-import { Tooltip,TooltipContent,TooltipProvider,TooltipTrigger } from '@/components/ui/tooltip';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { motion } from 'framer-motion';
+import { BsArrowDownRight } from 'react-icons/bs';
 
 const Resume = () => {
   return (
     <motion.div
-      initial={{opacity:0}}
+      initial={{ opacity: 0 }}
       animate={{
-        opacity:1,
-        transition:{delay:2.4,duration:0.4,ease:"easeIn"},
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
       className='min-h-[88vh] flex items-center justify-center py-12 xl:py-0'
     >
       <div className="container mx-auto">
         <Tabs defaultValue='experience' className='flex flex-col xl:flex-row gap-[60px]'>
           <TabsList className='flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6'>
-          <TabsTrigger value="experience">Experience</TabsTrigger>
-          <TabsTrigger value="education">Education</TabsTrigger>
-          <TabsTrigger value="skills">Skills</TabsTrigger>
-          <TabsTrigger value="about">About me</TabsTrigger>
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
           </TabsList>
 
           <div className='min-h-[70vh] w-full'>
+
             <TabsContent value='experience' className='w-full'>
               <div className='flex flex-col gap-[30px] text-center xl:text-left'>
                 <h3 className='text-4xl font-bold'>{experience.title}</h3>
-                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0 '>{experience.description}</p>
+                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{experience.description}</p>
                 <ScrollArea className="h-[400px]">
                   <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
-                    {experience.items.map((item,index)=>{
-                      return <li key={index} className='bg-[#232329] h-[184px] w-[450px] py-2 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1 ' >
-                        <span className='text-accent'>{item.duration}</span>
-                        <h3 className='text-xl min-h-[60px] text-center lg:text-left'>{item.position}</h3>
-                        <div className='flex items-center gap-3'>
-                          <span className='w-[6px] h-[6px] rounded-full bg-accent'></span>
-                          <p className='text-white/60'>{item.company}</p>
-                        </div>
-                      </li>
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li key={index} className='relative bg-[#232329] h-[184px] w-[450px] py-2 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
+                          <span className='text-accent'>{item.duration}</span>
+                          <h3 className='text-xl min-h-[60px] text-center lg:text-left'>{item.position}</h3>
+                          <div className='flex items-center gap-3'>
+                            <span className='w-[6px] h-[6px] rounded-full bg-accent'></span>
+                            <p className='text-white/60'>{item.company}</p>
+                          </div>
+
+                          <img src={item.logo} alt={`${item.company} logo`} className='absolute bottom-5 right-10 w-[50px] h-[50px] object-contain border-[2px]' />
+
+                          <div className='absolute top-5 right-5 group'>
+                            <a href={item.linkedin}
+                              target='_blank'
+                              rel='noopener noreferrer'
+                              className='w-[40px] h-[40px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
+                              <BsArrowDownRight className='text-primary text-xl' />
+                            </a>
+                          </div>
+                        </li>
+                      )
                     })}
                   </ul>
                 </ScrollArea>
-</div>
+              </div>
             </TabsContent>
-              
             <TabsContent value='education' className='w-full'>
-            <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+              <div className='flex flex-col gap-[30px] text-center xl:text-left'>
                 <h3 className='text-4xl font-bold'>{education.title}</h3>
-                {/* <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0 '>{education.description}</p> */}
                 <ScrollArea className="h-[400px]">
                   <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
-                    {education.items.map((item,index)=>{
+                    {education.items.map((item, index) => {
                       return <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
                         <span className='text-accent mt-3'>{item.position}</span>
                         <h3 className='text-xl max-w-[260px] min-h-[60px] font-bold  text-center lg:text-left mb-5 mt-3 '>{item.institution}</h3>
@@ -216,11 +231,11 @@ const Resume = () => {
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <div>
                   <h3 className='text-4xl font-bold'>{skills.title}</h3>
-                  
+
                 </div>
 
                 <ul className='grid grid-cols-2 sm:grid-cols-3 gap-4 md:grid-cols-4 xl:gap-[30px] mt-7'>
-                  {skills.skillList.map((skill,index)=>{
+                  {skills.skillList.map((skill, index) => {
                     return <li key={index}>
                       <TooltipProvider delayDuration={100}>
                         <Tooltip>
@@ -243,12 +258,12 @@ const Resume = () => {
                 <h3 className='text-4xl font-bold'>{about.title}</h3>
                 <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{about.description}</p>
                 <ul className='grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w[620px] mx-auto xl:mx-0'>
-                  {about.info.map((item,index)=>{
+                  {about.info.map((item, index) => {
                     return (
-                    <li key={index} className='flex items-center justify-center xl:justify-start gap-4'>
-                      <span className='text-white/60'>{item.fieldName}</span>
-                      <span className='text-xl'>{item.fieldValue}</span>
-                    </li>
+                      <li key={index} className='flex items-center justify-center xl:justify-start gap-4'>
+                        <span className='text-white/60'>{item.fieldName}</span>
+                        <span className='text-xl'>{item.fieldValue}</span>
+                      </li>
                     )
                   })}
                 </ul>
