cdbbc5a730efd669ad8f5eca438ba5236086d3e8
Niket Chawla <104648509+NC0312@users.noreply.github.com>
2024-10-31 13:43:55 +0530

    Update photo.jsx
    updated image for portfolio

--- components/ui/photo.jsx
+++ components/ui/photo.jsx
@@ -6,46 +6,48 @@ import Image from "next/image";
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
-      <motion.div 
+      <motion.div
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
           transition: { delay: 1, duration: 0.4, ease: "easeIn" },
         }}
       >
-        <motion.div 
+
+        
+        <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 1.4, duration: 0.4, ease: "easeIn" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] relative"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] relative overflow-hidden rounded-full p-1 bg-gradient-to-r from-black via-transparent to-black"
         >
-          {/* <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to-black opacity-50 z-10 rounded-full"></div> */}
           <Image
             src="/assets/profile-image.jpg"
             priority
             quality={100}
-            fill 
-            alt=""
+            fill
+            alt="Profile Image"
             className="object-contain rounded-full mix-blend-lighten"
           />
         </motion.div>
 
-        <motion.svg 
+
+        <motion.svg
           className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] absolute top-0 left-0"
-          fill="transparent" 
-          viewBox="0 0 506 506" 
+          fill="transparent"
+          viewBox="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <motion.circle 
-            cx="253" 
-            cy="253" 
-            r="250" 
-            stroke="#00ff99" 
-            strokeWidth="4" 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             initial={{ strokeDasharray: "24 10 0 0" }}
             animate={{
               strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
@@ -55,7 +57,7 @@ const Photo = () => {
               duration: 20,
               repeat: Infinity,
               repeatType: "reverse"
-            }} 
+            }}
           />
         </motion.svg>
       </motion.div>
