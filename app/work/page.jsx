a45170d7739cd2bfedb8b63d64615ef660f19a58
NC0312 <niketchawla983@gmail.com>
2024-11-10 00:18:43 +0530

    Tripify desc updated again

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -23,7 +23,7 @@ const projects=[
     num:"01",
     category:"Tripify",
     title:"Tripify",
-    description:"AI-powered travel planner built with React.js , Firebase , Google Maps API and Google Gemini recommendations",
+    description:"AI-powered travel planner built with React.js, Firebase,Google Maps API and Google Gemini API",
     stack:[{name:"ReactJS"},{name:"Firebase"},{name:"Gemini/Google Maps API"}],
     image:"/assets/work/tripify.png",
     live:"https://tripify-web.vercel.app/",
