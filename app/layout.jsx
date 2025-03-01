354a46dc584d3d4803c511528de10812fb34a70b
NC0312 <niketchawla983@gmail.com>
2025-03-01 18:32:29 +0530

    restricting the dev tools at prod

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import { icons } from "lucide-react";
 import PageTransition from "@/components/ui/PageTransition";
 import StairTransition from "@/components/ui/StairTransition";
+import { DevToolsBlocker } from "@/utils/devToolsBlocker";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"] , weight:["100","200","300","400","500","600","700","800"],
   variable:'--font-jetBrainsMono'
@@ -18,6 +19,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const isProduction = process.env.NEXT_PUBLIC_ENV === 'production';
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable}>
@@ -26,6 +28,7 @@ export default function RootLayout({ children }) {
         <PageTransition>
         {children}
         </PageTransition>
+        {isProduction && <DevToolsBlocker />}
         </body>
     </html>
   );
